15483441bd77c3578c4798959c502c7e37c358e3
Rasel Kibria <rasel5jkibria@gmail.com>
2021-09-28 19:38:05 +0600

    nevbar added

---
+++ TASK_2/resources/views/include/topNevBar.blade.php
@@ -0,0 +1,3 @@
+<a class="btn btn-primary" href="{{route('login')}}">Login</a>
+<a class="btn btn-success" href="{{route('registration ')}}">Registration </a>
+<a class="btn btn-danger" href="{{route('contact ')}}"> Contact  </a>
