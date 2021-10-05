456af6ceb395c45dabd25064bcf86c3b55a1fc5c
Rasel Kibria <rasel5jkibria@gmail.com>
2021-10-05 12:06:16 +0600

    nevbar and basic layout added

---
+++ TASK_03/resources/views/inc/topnav.blade.php
@@ -0,0 +1,4 @@
+<a class="btn btn-primary" href="{{route('add')}}">Add Product</a>
+<a class="btn btn-success" href="{{route('list')}}">Product list</a>
+<a class="btn btn-danger" href="{{route('update')}}"> Update Product </a>
+<a class="btn btn-danger" href="{{route('delete')}}"> Delete Product </a>
